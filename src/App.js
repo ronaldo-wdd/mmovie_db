ece6a5ef330a9dca485b8384a625a8713fe12af9
Ronaldo Silva <ronasbr59@gmail.com>
2021-01-16 17:52:13 +0000

    firebase config

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <b>MMovie_DB</b> - My Movie database | Powered by <b>TMDB</b>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );

2d8fd4c95863ddbcd0942a4e94e532dea86dddca
Ronaldo Silva <ronasbr59@gmail.com>
2021-01-20 03:22:13 +0000

    carousel transition

--- src/components/Sections/MainMovie/Media/Backdrop/Backdrop.js
+++ src/components/Sections/MainMovie/Media/Backdrop/Backdrop.js
@@ -1,30 +1,88 @@
-import React from 'react';
-import classes from './Backdrop.module.css';
+import React, { Component } from 'react';
 import Col from 'react-bootstrap/Col';
+import { connect } from 'react-redux';
+import CSSTransition from 'react-transition-group/CSSTransition';
+import TransitionGroup from 'react-transition-group/TransitionGroup';
 
-import backdropImg from '../../../../../assents/images/backdrop.jpg';
+import './Backdrop.css';
+import classes from './Backdrop.module.css';
+import * as actions from '../../../../../store/actions';
 
 
-const Backdrop = () => {
-    return (
-        <div className={classes.Carousel} >
-            <Col md='10' 
-                className={classes.Backdrop}
-                style={{
-                    // background: `url(${mainImage}) no-repeat center`,
-                    backgroundImage: `url(${backdropImg})`,
-                    backgroundRepeat: 'no-repeat',
-                    backgroundPosition: 'center top',
-                    backgroundSize: 'cover'
-                }} />
-            <Col md="5"
-                className={classes.NextBackdrop}
-                />
-            <Col md="4"
-                className={classes.FakeBackdrop}
-                />
-        </div>
-    );
+class Backdrop extends Component {
+    state = {
+        mainMovies: [],
+        loading: true
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return (this.props !== nextProps || JSON.stringify(this.state) !== JSON.stringify(nextState)) 
+            ? true : false;
+    }
+
+    componentDidUpdate () {
+        if (this.props.movies != null) {
+            const mainMovies = [...this.props.movies.results].slice(
+                this.props.activeMovie,
+                this.props.activeMovie + 3);
+
+            mainMovies.unshift([...this.props.movies.results].slice(
+                this.props.activeMovie - 1
+            )[0]);
+                
+            this.setState({mainMovies: mainMovies, loading: false});
+            console.log(this.state.mainMovies);
+        }
+    }
+    
+    render () {
+        const displayedMovies = [];
+        const backdropsConfig = [
+            {md: '10', class: classes.PrevBackdrop},
+            {md: '10', class: classes.Backdrop},
+            {md: '5', class: classes.NextBackdrop},
+            {md: '4', class: classes.FakeBackdrop},
+        ];
+
+        this.state.mainMovies.forEach((el, key) => {
+            displayedMovies.push(
+                <CSSTransition
+                    key={el.id}
+                    timeout={700}
+                    classNames="slide" >
+                    <Col md={backdropsConfig[key].md}
+                        className={backdropsConfig[key].class} >
+                        <img src={`https://image.tmdb.org/t/p/original${el.backdrop_path}`} />
+                    </Col>
+                </CSSTransition>
+            )
+        });
+        
+        return (
+
+            !this.state.loading &&
+
+            <div className={classes.Carousel} onClick={()=>this.props.onSetActiveMovie(this.props.activeMovie + 1)} >
+                <TransitionGroup component={null}>
+                    {displayedMovies}
+                </TransitionGroup>
+            </div>
+        );
+    }
 }
 
-export default Backdrop;
+
+const mapStateToProps = state => {
+    return {
+        activeMovie: state.activeMovie,
+        movies: state.movies
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onSetActiveMovie: index => dispatch(actions.activeMovie(index))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Backdrop);

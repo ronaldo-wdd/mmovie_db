784e3a47f829f8fa7f7419f44719858536808ec6
Ronaldo Silva <ronasbr59@gmail.com>
2021-01-22 15:29:54 +0000

    mobile playTrailer button and movie title added

--- src/components/Sections/MainMovie/MainMovie.js
+++ src/components/Sections/MainMovie/MainMovie.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import classes from './MainMovie.module.css';
+import { connect } from 'react-redux';
+import Container from 'react-bootstrap/Container';
 
 import Media from './Media/Media';
 import Info from './Info/Info';
+import playBtn from '../../../assents/svg/play.svg';
 
 
-const MainMovie = () => {
+const MainMovie = props => {
+    const movie = props.movies[props.activeMovie];
+    
     return (
         <div className={classes.MainMovie}>
             <Media />
+            {props.isMobile
+                ? !props.loading &&
+                    <Container fluid="xxl" className={classes.Info}>
+                        <img src={playBtn} alt="" />
+                        <h1>{movie.title}</h1>
+                    </Container>
+                : !props.loading && <Info mobile={false} />
+            }
         </div>
     );
 }
 
-export default MainMovie;
+
+const mapStateToProps = state => {
+    return {
+        isMobile: state.navigation.mobile,
+        loading: state.navigation.loading,
+        movies: state.movies.movies,
+        activeMovie: state.movies.activeMovie
+    }
+}
+
+export default connect(mapStateToProps)(MainMovie);

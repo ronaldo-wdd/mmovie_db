61b48b6ef7859d04440237754df50794f0c7ff1c
Ronaldo Silva <ronasbr59@gmail.com>
2021-01-22 15:36:19 +0000

    state passed to reducer

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from "react-redux";
 
 import Layout from './hoc/Layout/Layout';
 import Header from './components/Navigation/Header/Header';
 import Main from './containers/Main/Main';
+import * as actions from './store/actions';
 // import AllMovies from './containers/AllMovies/AllMovies';
 
 
 class App extends Component {
-  state = {
-    isMobile: false
-  }
 
   // isMobile?
   changeMobile = () => {
     window.matchMedia('(max-width: 768px)').matches
-      ? this.setState({isMobile: true})
-      : this.setState({isMobile: false})
+      ? this.props.onChangeMobile(true)
+      : this.props.onChangeMobile(false)
   }; 
 
   componentDidMount () {
     this.changeMobile();
     window.addEventListener('resize', this.changeMobile);
+    this.props.onFetchMoviesGenre();
+    this.props.onFetchMovies();
     // return () => window.removeEventListener('resize', this.changeMobile);
   }
 
@@ -29,11 +30,27 @@ class App extends Component {
   render() {
     return (
       <Layout>
-        <Header isMobile={this.state.isMobile} />
+        <Header isMobile={this.props.isMobile} />
         <Route path="/" component={Main} />
       </Layout>
     );
   }
 }
 
-export default App;
+
+const mapStateToProps = state => {
+  return {
+    isMobile: state.navigation.mobile,
+    loading: state.navigation.loading
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onChangeMobile: mobile => dispatch(actions.change_mobile(mobile)),
+    onFetchMovies: () => dispatch(actions.fetch_movies()),
+    onFetchMoviesGenre: () => dispatch(actions.fetch_movies_genre())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);

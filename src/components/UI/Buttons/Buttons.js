aa8f1273334a1e967b76cb6579952aabe02e81f7
Ronaldo Silva <ronasbr59@gmail.com>
2021-01-22 15:23:56 +0000

    nextBtn, prevBtn, showAllBtn - create

--- src/components/UI/Buttons/Buttons.js
+++ src/components/UI/Buttons/Buttons.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import classes from './Buttons.module.css';
 
-const Buttons = () => {
-    return (
-        <div></div>
-    );
+
+const Buttons = props => {
+    switch(props.type) {
+        case "next": return (
+            <div className={classes.NextBtn}
+                onClick={props.click} >
+                <p>Next</p>
+            </div> );
+        case "goBack": return (
+            <div className={classes.BackBtn}
+                onClick={props.click} >
+                <p>Go Back</p>
+            </div> );
+        case "showAll": return (
+            <div className={classes.ShowAllBtn}
+                onClick={props.click} >
+                <p>Show All</p>
+            </div> );
+        default: return (
+            <div className={classes.Button}
+                onClick={props.click} >
+                <p>{props.children}</p>
+            </div> );
+    }
 }
 
-export default Buttons();
+export default Buttons;

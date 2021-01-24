b5b98fb3932555ce6d80a35958f5abdc667222a1
Ronaldo Silva <ronasbr59@gmail.com>
2021-01-24 19:14:39 +0000

    search page creating

---
+++ src/containers/Search/Search.js
@@ -0,0 +1,13 @@
+import classes from './Search.module.css';
+import React from 'react';
+
+
+const Search = () => {
+    return (
+        <div className={classes.Search}>
+
+        </div>
+    );
+}
+
+export default Search;

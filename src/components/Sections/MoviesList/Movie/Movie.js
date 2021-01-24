b7bc1aff0ec5d0eac5343b971c503d6c904245dc
Ronaldo Silva <ronasbr59@gmail.com>
2021-01-24 14:27:35 +0000

    movie component with fetched data

--- src/components/Sections/MoviesList/Movie/Movie.js
+++ src/components/Sections/MoviesList/Movie/Movie.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import classes from './Movie.module.css';
+import Col from 'react-bootstrap/Col';
 
-import moviePoster from '../../../../assents/images/poster.jpg';
+import defaultPosterPath from '../../../../assents/images/defaultPoster.jpg';
 
-const Movie = () => {
+
+const Movie = props => {
+    const movie = props.movie;
+    let movieClasses = [classes.Movie];
+    props.active && movieClasses.push(classes.Active);
+
     return (
-        <div className={classes.Movie}>
-            <img src={moviePoster} alt="" title="" />
-        </div>
+        <Col xs="6" sm="4" md="3" lg="2" 
+            ref={props.ref}
+            className={movieClasses.join(' ')}
+            onClick={props.click} >
+            <img alt={movie.title}
+                src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} 
+                onError = {e => e.target.src = {defaultPosterPath}} />
+        </Col>
     );
 }
 

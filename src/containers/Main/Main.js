8e9606f355d498763a56df5331f4e5a969913b46
Ronaldo Silva <ronasbr59@gmail.com>
2021-01-25 00:08:55 +0000

    Carrousel 10 main movies fixed

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,21 +1,45 @@
 import React, {Component} from 'react';
 import classes from './Main.module.css';
-
 import { connect } from 'react-redux';
+import * as actions from '../../store/actions';
 
 import MainMovie from '../../components/Sections/MainMovie/MainMovie';
 import MoviesList from '../../components/Sections/MoviesList/MoviesList';
-import Info from '../../components/Sections/MainMovie/Info/Info';
 
 
 class Main extends Component {
+    state = {
+        movies: []
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return (this.props !== nextProps || JSON.stringify(this.state) !== JSON.stringify(nextState)) 
+            ? true : false;
+    }
+    
+    componentDidUpdate () {
+        let movies = [];
+        if (this.props.history.location.pathname === '/movies') {
+            this.props.onShowAllMovies(true);
+            movies = this.props.movies;
+        }
+
+        else {
+            this.props.onShowAllMovies(false);
+            movies = this.props.movies.slice(0, 10);
+        }
+
+        this.setState({movies: movies});
+    }
+    
     render() {
         return (
             <div className={classes.Main}>
-                <MainMovie />
-                { (this.props.isMobile && !this.props.loading) 
-                    && (<Info mobile={true} />) }
-                {!this.props.showMoreDetails && <MoviesList />}
+                <MainMovie movies={this.state.movies} />
+                {!this.props.showMoreDetails 
+                    && <MoviesList 
+                        history={this.props.history} 
+                        movies={this.state.movies} />}
             </div>
         );
     }
@@ -26,8 +50,15 @@ const mapStateToProps = state => {
     return {
         isMobile: state.navigation.mobile,
         loading: state.navigation.loading,
-        showMoreDetails: state.navigation.showMovieDetails
+        showMoreDetails: state.navigation.showMovieDetails,
+        movies: state.movies.movies
     }
 }
 
-export default connect(mapStateToProps)(Main);
+const mapDispatchToProps = dispatch => {
+    return {
+        onShowAllMovies: show => dispatch(actions.show_all_movies(show))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);

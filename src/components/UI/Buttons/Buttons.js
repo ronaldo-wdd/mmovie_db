3aad3639f7bb708198c51b609646e92d324145fa
Ronaldo Silva <ronasbr59@gmail.com>
2021-02-03 22:59:15 +0000

    buttons multiclass suport

--- src/components/UI/Buttons/Buttons.js
+++ src/components/UI/Buttons/Buttons.js
@@ -5,22 +5,22 @@ import classes from './Buttons.module.css';
 const Buttons = props => {
     switch(props.type) {
         case "next": return (
-            <div className={classes.NextBtn}
+            <div className={[classes.NextBtn, props.className].join(' ')}
                 onClick={props.click} >
                 <p>Next</p>
             </div> );
         case "goBack": return (
-            <div className={classes.BackBtn}
+            <div className={[classes.BackBtn, props.className].join(' ')}
                 onClick={props.click} >
                 <p>Go Back</p>
             </div> );
         case "showAll": return (
-            <div className={classes.ShowAllBtn}
+            <div className={[classes.ShowAllBtn, props.className].join(' ')}
                 onClick={props.click} >
                 <p>Show All</p>
             </div> );
         default: return (
-            <div className={classes.Button}
+            <div className={[classes.Button, props.className].join(' ')}
                 onClick={props.click} >
                 <p>{props.children}</p>
             </div> );

092a8904d1725d5d0dbdc3fbf4f8244716e92d09
Ronaldo Silva <ronasbr59@gmail.com>
2021-02-07 15:35:16 +0000

    add updateScroll resucer

--- src/store/reducers/navigation.js
+++ src/store/reducers/navigation.js
@@ -1,10 +1,11 @@
 import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
-    loading: true,
+    loaded: false,
     mobile: false,
     showMovieDetails: false,
-    showAllMovies: false
+    showAllMovies: false,
+    moviesListScrollPosition: 0
 }
 
 
@@ -15,10 +16,10 @@ const changeMobile = (state, mobile) => {
     }
 }
 
-const isLoading = (state, loading) => {
+const is_loaded = (state, loaded) => {
     return {
         ...state,
-        loading: loading
+        loaded: loaded
     }
 }
 
@@ -36,13 +37,21 @@ const showAllMovies = (state, show) => {
     }
 }
 
+const updateScrollPosition = (state, position) => {
+    return {
+        ...state,
+        moviesListScrollPosition: position
+    }
+}
+
  
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.CHANGE_MOBILE: return changeMobile(state, action.mobile);
-        case actionTypes.IS_LOADING: return isLoading(state, action.loading);
+        case actionTypes.LOADED: return is_loaded(state, action.loaded);
         case actionTypes.SHOW_MOVIE_DETAILS: return showMovieDetails(state, action.show);
         case actionTypes.SHOW_ALL_MOVIES: return showAllMovies(state, action.show);
+        case actionTypes.UPDATE_SCROLL_POSITION: return updateScrollPosition(state, action.position);
         default: return state;
     }
 }

0731d3b84136486a396521c6c3dc0f2f30cce3d9
Ronaldo Silva <ronasbr59@gmail.com>
2021-02-20 20:29:06 +0000

    add: image wrapper

--- src/components/Sections/MoviesList/Movie/Movie.js
+++ src/components/Sections/MoviesList/Movie/Movie.js
@@ -7,17 +7,19 @@ import defaultPosterPath from '../../../../assents/images/defaultPoster.jpg';
 
 const Movie = props => {
     const movie = props.movie;
-    let movieClasses = [classes.Movie];
-    props.active && movieClasses.push(classes.Active);
+    let colClasses = [classes.Col];
+    props.active && colClasses.push(classes.Active);
 
     return (
         <Col xs="6" sm="4" md="3" lg="2" 
             ref={props.ref}
-            className={movieClasses.join(' ')}
+            className={colClasses.join(' ')}
             onClick={props.click} >
-            <img alt={movie.title}
-                src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} 
-                onError = {e => e.target.src = defaultPosterPath} />
+            <div className={classes.Movie}>
+                <img alt={movie.title}
+                    src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} 
+                    onError = {e => e.target.src = defaultPosterPath} />
+            </div>
         </Col>
     );
 }

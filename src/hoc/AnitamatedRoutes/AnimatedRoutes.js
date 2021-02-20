d4b521b2c68e87569a647bc11b54c9469790f7b6
Ronaldo Silva <ronasbr59@gmail.com>
2021-02-20 20:33:49 +0000

    add: Movies route

--- src/hoc/AnitamatedRoutes/AnimatedRoutes.js
+++ src/hoc/AnitamatedRoutes/AnimatedRoutes.js
@@ -5,6 +5,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import Main from '../../containers/Main/Main';
 import Search from '../../components/Pages/Search/Search';
+import Movies from '../../components/Pages/Movies/Movies';
 import Movie from '../../components/Pages/Movie/Movie';
 
 
@@ -20,6 +21,7 @@ const AnimatedRoutes = props => {
                 <Switch>
                     <Route path="/search" component={Search} />
                     <Route path="/movie/:id" component={Movie} />
+                    <Route path="/movies/:id" component={Movies} />
                     <Route path="/" component={Main} />
                 </Switch>
             </Transition>

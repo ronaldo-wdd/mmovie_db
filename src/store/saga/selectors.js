2d336f2870ba085658754e8030fa834315b56dbd
Ronaldo Silva <ronasbr59@gmail.com>
2021-02-20 20:46:03 +0000

    add: params

--- src/store/saga/selectors.js
+++ src/store/saga/selectors.js
@@ -3,3 +3,4 @@ export const moviesResults = state => state.movies.movies.length;
 export const activeFilter = state => state.movies.activeFilter;
 export const totalPages = state => state.movies.fetchedMovies.total_pages;
 export const page = state => state.movies.fetchedMovies.page;
+export const params = state => state.movies.params;

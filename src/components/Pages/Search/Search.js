d170110f9c7e169e43766a295bc10e87ecf27a08
Ronaldo Silva <ronasbr59@gmail.com>
2021-02-23 14:15:18 +0000

    add: handleSearch funcs & display results

--- src/components/Pages/Search/Search.js
+++ src/components/Pages/Search/Search.js
@@ -3,16 +3,74 @@ import React, { Component } from 'react';
 
 import MoviesList from '../../Sections/MoviesList/MoviesList';
 import TopNav from '../../Navigation/TopNav/TopNav';
+import { connect } from 'react-redux';
+import * as actions from '../../../store/actions';
+
 
 class Search extends Component {
+    state = { movies: [] }
+
+    componentDidMount() {        
+        this.props.onSetParams({
+            genre: undefined, 
+            year: undefined, 
+            query: this.props.query || ''
+        });
+    }
+
+    componentWillUnmount() {
+        const pathname = this.props.history.location.pathname.split('/')[1];
+        if (pathname !== 'movie') {
+            this.props.onSetParams({query: undefined});
+            this.props.onFetchMovies();
+        }
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return (this.props !== nextProps || JSON.stringify(this.state) !== JSON.stringify(nextState)) 
+            ? true : false;
+    }
+    
+    componentDidUpdate() {
+        (this.props.loaded && (this.props.query || this.props.query !== ''))
+                && this.setState({movies: this.props.movies, searching: false});
+    }
+    
+    handleInputChange(event) {
+        this.props.onLoading();
+        this.props.onSetParams({query: event.target.value});
+    }
+
+    handleFormSubmit(event) {
+        this.setState({searching: true});
+        event.preventDefault();
+        this.props.onFetchMovies();
+    }
+
     render () {
         return (
             <div className={classes.Search}>
-                <TopNav search={true} history={this.props.history} />
-                {/* <MoviesList history={props.history} /> */}
+                <TopNav search={true} 
+                    history={this.props.history}
+                    handleInputChange={this.handleInputChange.bind(this)}
+                    handleFormSubmit = {this.handleFormSubmit.bind(this)} />
+                <MoviesList movies={this.state.movies}
+                    history={this.props.history} />
             </div>
         );
     }
 }
 
-export default Search;
+
+const mapStateToProps = state => ({ 
+    movies: state.movies.movies,
+    query: state.movies.params.query,
+    loaded: state.navigation.loaded
+}),
+    mapDispatchToProps = dispatch => ({ 
+        onSetParams: params => dispatch(actions.set_params(params)),
+        onFetchMovies: ()=> dispatch(actions.fetch_movies()),
+        onLoading: ()=> dispatch(actions.loaded(false))
+    });
+
+export default connect(mapStateToProps, mapDispatchToProps)(Search);

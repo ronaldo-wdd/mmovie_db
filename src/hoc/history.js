5f807d77925aa8d42b5dc67148f3b87cbd6a683e
Ronaldo Silva <ronasbr59@gmail.com>
2021-02-26 16:33:13 +0000

    add: update store on history change func

--- src/hoc/history.js
+++ src/hoc/history.js
@@ -1,9 +1,13 @@
 import { createBrowserHistory } from 'history';
+import store from '../store';
+import * as actions from '../store/actions';
 
 const history = createBrowserHistory();
 
-history.listen((location, action) => {
-    console.log(action, "=>", location.pathname)
+history.listen(location => {
+    let page = location.pathname.split('/')[1];
+    
+    store.dispatch(actions.curr_page(page));
 });
 
 export default history;

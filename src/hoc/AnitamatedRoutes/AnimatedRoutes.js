e411bf9e773612e695ba662fbb1b4b92bd978713
Ronaldo Silva <ronasbr59@gmail.com>
2021-08-13 17:40:35 +0100

    add: login page route

--- src/hoc/AnitamatedRoutes/AnimatedRoutes.js
+++ src/hoc/AnitamatedRoutes/AnimatedRoutes.js
@@ -7,10 +7,11 @@ import Main from '../../containers/Main/Main';
 import Search from '../../components/Pages/Search/Search';
 import Movies from '../../components/Pages/Movies/Movies';
 import Movie from '../../components/Pages/Movie/Movie';
+import Auth from '../../components/Pages/Auth/Auth';
 
 import { fadeOut, play } from '../../shared/Timelines/pageTransition';
 
-
+ 
 const AnimatedRoutes = props => {
     const pathname = props.location.pathname.split('/')[1] || 'home', 
         key = props.location.pathname.split('/')[1] || 'home';
@@ -27,6 +28,7 @@ const AnimatedRoutes = props => {
                     <Route path="/search" component={Search} />
                     <Route path="/movie/:id" component={Movie} />
                     <Route path="/movies/:id" component={Movies} />
+                    <Route path="/login" component={Auth} />
                     <Route path="/" component={Main} />
                     <Redirect to="/" />
                 </Switch>
